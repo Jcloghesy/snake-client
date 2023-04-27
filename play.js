29c62826fec240e4a57b126cd815c75f185e5980
John <john.cloghesy@gmail.com>
2023-04-27 01:15:58 -0600

    Input Module: Modularize input functions & refactor | add input module
    - Add module to contain all user input - `input.js`
    - Move `handleUserInput` & `setupInput` functions from `play.js` to `input.js`
    - Export both functions from `input.js` and required in `play.js`

--- play.js
+++ play.js
@@ -2,52 +2,19 @@
  *
  *
  *  - play.js   - the main file where the game client interacts with server
- *  -           - contains setupInput function to process user input  
+ *              - requires connect function from client.js
+ *              - requires setupInputs function from input.js
  *  - client.js - a separate file /module for TCP connections
- *  -           - references / inputs the connect function 
+ *              - references / inputs the connect function 
+ *  - input.js  - a separate module for all user inputs
  */
 
 /**   **** Requires the `connect` Function from `client.js` **** */  
-const {connect} = require('./client')
-
-/** ******** Implements User Interface Functions ********
- *   - Implements initial steps in User interface to handle keyboard input
- *   - Allow players to send keyboard commands to server via UI
- *   - `stdin` object acts as an event listener 
- *   - Implements two functions associated with stdin object 
- *   -  1. Implement setupInput to received user input from keyboard
- *   -  2. Implement handleUserInput that specifies action once input received 
- *   - Implement exiting the game upon user keying  ctrl + c  
- */
-
-/**  *** Implements handleUserInput function *** 
- *   - Function specifies action to take once input(keyboard input) is received 
- *   - 1. Implements 1st action exiting the game upon user keying  ctrl + c  
- */
-const handleUserInput = function () {
-  if (key === "\u0003") {
-    console.log('You have quit the current session and exited the game.')
-    process.exit();
-  }
-};
-/** *** Implements setupInput function *** 
- *   - function allows for user input from keyboard to be received 
- *   - contains general conditions (RawMode, Encoding, resume)
- *   - contains/references handleUserInput 
- */
-const setupInput = function () {
-  const stdin = process.stdin;
-    stdin.setRawMode(true);
-    stdin.setEncoding("utf8");
-    stdin.resume();
-
-    stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-setupInput ();
+const { connect } = require('./client');
+const { setupInput } = require('./input');
 
 /** Logs message to console client is attempting to connect to server */
 console.log("Connecting ...");
 
 connect();
+setupInput();
