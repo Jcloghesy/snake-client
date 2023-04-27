b5e075865257e13cc5aef21845778f1bad7ccf4e
John <john.cloghesy@gmail.com>
2023-04-26 23:28:42 -0600

    Connecting to Server: Implement `connect` function in `play.js`
    - Add function named `connect` to establish connection with game server
    - Use `Node's` `net` library and `createConnection` function for `connect`
    - Assumes `localhost` as host IP address when connecting
    - Immediately logs message to client/player "Connecting …" until established

--- play.js
+++ play.js
@@ -2,3 +2,29 @@
  *
  *  - File serves as the main file through which the game client is launched
  */
+
+/** **** IMPLEMENT `connect` FUNCTION ****
+ *     - `connect` function establishes a connection with the game server
+ *     - Immediately logs message to client "Connecting …" until connected
+ *     - Assumes `localhost` as host IP address when connecting 
+ *     - Assumes port 50541 
+ */
+const net = require("net");
+
+/** *** CONNECT - establishes a connection with the game server *** */
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost", /* IP address here  */
+    port: 50541,       /* PORT number here */
+  });
+
+  /** interpret incoming data as text */
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+/** Log message to console client is attempting to connect to server */
+console.log("Connecting ...");
+
+connect();
